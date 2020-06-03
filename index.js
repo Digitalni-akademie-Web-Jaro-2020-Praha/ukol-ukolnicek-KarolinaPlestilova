314236507e4da758ce4da6a8f005ab8f2a8dbc89
KarolinaPlestilova <k.plestilova@gmail.com>
2020-06-03 14:34:53 +0200

    All done

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 'use strict';
 
 console.log('funguju!');
+
+const tasks = ['Koupit papriku', 'Shlédnout videa DA', 'Napsat PR článek', 'Dodělat úkoly'];
+const updateTasks = () => {
+  const tasksElm = document.querySelector('.todo__tasks');
+  tasksElm.innerHTML = '';
+  for (let i = 0; i < tasks.length; i++) {
+    tasksElm.innerHTML += `<div class="task">${tasks[i]}</div>`;
+  }
+}
+updateTasks()
+const addNewTask = () => {
+  const input = document.querySelector('#new-task').value;
+  if (input === '') {
+    alert("Zadej, co chceš zadat na seznam!!!");
+  } else {
+    updateTasks(tasks.push(input))
+  }
+}
+const btnAdd = document.querySelector('.btn-add');
+btnAdd.addEventListener('click', addNewTask);
